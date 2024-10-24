01f50335d4d288a46559384dd8f9f8c8e4cb978b
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2024-10-23 20:02:32 -0400

    23/10

--- js/Lesson1/lesson1.js
+++ js/Lesson1/lesson1.js
@@ -100,3 +100,189 @@ function Task10() {
   makeMessage("Reactor", 8000);
   makeMessage("Engine", 4070);
 }
+
+function Task11() {
+  function calculateTotalPrice(orderedQuantity, pricePerItem) {
+    // Change code below this line
+    const totalPrice = orderedQuantity * pricePerItem;
+
+    // Change code above this line
+    return totalPrice;
+  }
+
+  calculateTotalPrice(5, 100);
+  calculateTotalPrice(8, 60);
+  calculateTotalPrice(3, 400);
+  calculateTotalPrice(1, 3500);
+  calculateTotalPrice(12, 70);
+}
+
+function Task12() {
+  function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+    // Change code below this line
+    const message = `You ordered droids worth ${
+      orderedQuantity * pricePerDroid + deliveryFee
+    } credits. Delivery (${deliveryFee} credits) is included in total price.`;
+    // Change code above this line
+    return message;
+  }
+
+  makeOrderMessage(2, 100, 50);
+  makeOrderMessage(4, 300, 100);
+  makeOrderMessage(10, 70, 200);
+}
+
+function Task13() {
+  function isAdult(age) {
+    // Change code below this line
+    const passed = age >= 18;
+
+    // Change code above this line
+    return passed;
+  }
+
+  isAdult(20);
+  isAdult(14);
+  isAdult(8);
+  isAdult(37);
+}
+
+function Task14() {
+  function isValidPassword(password) {
+    const SAVED_PASSWORD = "jqueryismyjam";
+    // Change code below this line
+    const isMatch = SAVED_PASSWORD === password;
+
+    // Change code above this line
+    return isMatch;
+  }
+
+  isValidPassword("mangodab3st");
+  isValidPassword("kiwirul3z");
+  isValidPassword("jqueryismyjam");
+}
+
+function Task15() {
+  function checkAge(age) {
+    let message;
+
+    if (age >= 18) {
+      // Change this line
+      message = "You are an adult";
+    } else {
+      message = "You are a minor";
+    }
+
+    return message;
+  }
+
+  checkAge(20);
+  checkAge(8);
+  checkAge(14);
+  checkAge(38);
+}
+
+function Task16() {
+  function checkStorage(available, ordered) {
+    let message;
+    // Change code below this line
+    if (ordered > available) {
+      message = "Not enough goods in stock!";
+    } else {
+      message = "Order is processed, our manager will contact you.";
+    }
+
+    // Change code above this line
+    return message;
+  }
+
+  checkStorage(100, 50);
+  checkStorage(100, 130);
+  checkStorage(200, 20);
+  checkStorage(200, 150);
+  checkStorage(150, 180);
+}
+
+function Task17() {
+  let a = 5;
+  let b = 10;
+  let c = 15;
+  let d = 20;
+
+  // Change code below this line
+  a += 2;
+  b -= 4;
+  c *= 3;
+  d /= 10;
+}
+
+function Task18() {
+  function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+    let message;
+    // Change code below this line
+    const totalPrice = pricePerDroid * orderedQuantity;
+
+    if (totalPrice > customerCredits) {
+      message = "Insufficient funds!";
+    } else {
+      message = `You ordered ${orderedQuantity} droids, you have ${
+        customerCredits - totalPrice
+      } credits left`;
+    }
+    // Change code above this line
+    return message;
+  }
+
+  makeTransaction(3000, 5, 23000);
+  makeTransaction(1000, 3, 15000);
+  makeTransaction(5000, 10, 8000);
+  makeTransaction(2000, 8, 10000);
+  makeTransaction(500, 10, 5000);
+}
+
+function Task19() {
+  function checkPassword(password) {
+    const ADMIN_PASSWORD = "jqueryismyjam";
+    let message;
+
+    if (!password) {
+      // Change this line
+      message = "Canceled by user!";
+    } else if (password === ADMIN_PASSWORD) {
+      // Change this line
+      message = "Welcome!";
+    } else {
+      message = "Access denied, wrong password!";
+    }
+
+    return message;
+  }
+
+  checkPassword("mangohackzor");
+  checkPassword(null);
+  checkPassword("polyhax");
+  checkPassword("jqueryismyjam");
+}
+
+function Task20() {
+  function checkStorage(available, ordered) {
+    let message;
+    // Change code below this line
+    if (!ordered) {
+      message = "There are no products in the order!";
+    } else if (ordered > available) {
+      message = "Your order is too large, there are not enough items in stock!";
+    } else {
+      message = "The order is accepted, our manager will contact you";
+    }
+    // Change code above this line
+    return message;
+  }
+
+  checkStorage(100, 50);
+  checkStorage(100, 130);
+  checkStorage(70, 0);
+  checkStorage(200, 20);
+  checkStorage(200, 250);
+  checkStorage(150, 0);
+}
