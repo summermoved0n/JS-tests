c1cf02c6f5c30fd6326d478b73debcc7273728e5
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2024-10-26 21:21:16 -0400

    26/10

---
+++ js/Lesson1/repeta1.js
@@ -0,0 +1,15 @@
+//? Problem 1
+
+// let firstNumber = prompt("Fill in first number");
+// firstNumber = Number(firstNumber);
+
+// let secondNumber = prompt("Fill in second number");
+// secondNumber = Number(secondNumber);
+
+// const result = Math.pow(firstNumber, secondNumber);
+// console.log(result);
+// 8;
+
+const random = Math.random();
+
+console.log(random, Math.round(10 + random));
