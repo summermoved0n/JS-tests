141a47874de18e561f1f3fb4858008c2aeae8f8e
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2024-10-27 11:03:45 -0400

    27/10 p1

--- js/Lesson1/repeta1.js
+++ js/Lesson1/repeta1.js
@@ -10,6 +10,30 @@
 // console.log(result);
 // 8;
 
-const random = Math.random();
+// const random = Math.random();
+// console.log(random, Math.round(10 + random));
 
-console.log(random, Math.round(10 + random));
+// ? Problem 2
+
+// const firstName = "Vitya";
+// const lastName = "Dinango";
+
+// const room = 671;
+// const type = "VIP";
+
+// const welcomeMsg = `${firstName} ${lastName} check in room ${room + type}`;
+
+// console.log(welcomeMsg);
+
+// ? Problem 3
+
+// const free = "free";
+// const pro = "pro";
+// const vip = "vip";
+
+// const subscription = free;
+
+// const canAccessContent = subscription === pro || subscription === vip;
+// console.log("Can enter the system?: ", canAccessContent);
+
+// ? Problem 4
