e34780b23f36a55bdb39a184598c3c46abba3e6a
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2024-10-28 20:15:34 -0400

    28/10

--- js/Lesson1/repeta1.js
+++ js/Lesson1/repeta1.js
@@ -37,3 +37,76 @@
 // console.log("Can enter the system?: ", canAccessContent);
 
 // ? Problem 4
+
+// const stars = 0;
+// let price;
+
+// switch (stars) {
+//   case 1:
+//     price = "20$";
+//     break;
+//   case 2:
+//     price = "30$";
+//     break;
+//   case 3:
+//     price = "50$";
+//     break;
+//   case 4:
+//     price = "70$";
+//     break;
+//   case 5:
+//     price = "120$";
+//     break;
+//   default:
+//     price = "This number not in a list";
+// }
+
+// if (stars === 1 || stars === 2) {
+//   price = "20$";
+// } else if (stars === 3 || stars === 4) {
+//   price = "50$";
+// } else if (stars === 5) {
+//   price = "100$";
+// } else {
+//   price = "Use another number";
+// }
+
+// console.log(price);
+
+// ? Problem 5
+
+// for (let i = 1; i <= 20; i = i + 1) {
+//   console.log(i);
+// }
+
+// const minMoney = 500;
+// const maxMoney = 5000;
+// const workers = 5;
+// let totalSalery = 0;
+
+// for (let i = 1; i <= workers; i += 1) {
+//   const randomSalry = Math.round(
+//     Math.random() * (maxMoney - minMoney) + minMoney
+//   );
+//   console.log(randomSalry);
+//   totalSalery = totalSalery + randomSalry;
+// }
+
+// console.log(totalSalery);
+
+// ? Problem 6
+
+// const min = 3;
+// const max = 23;
+// let total = 0;
+
+// for (let i = min; i <= max; i += 1) {
+//   console.log("Порядковий номер ", i);
+//   const neededNumber = i % 2;
+//   if (neededNumber === 0) {
+//     total += i;
+//     console.log("total ", total);
+//   }
+// }
+
+// console.log("all sum ", total);
