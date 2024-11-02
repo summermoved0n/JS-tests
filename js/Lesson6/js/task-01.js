c63bf36f0a7337d957adef9f5fb5f530a6d537cb
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2024-11-01 20:36:24 -0400

    test less6

---
+++ js/Lesson6/js/task-01.js
@@ -0,0 +1,29 @@
+const textAnimals = document.getElementById("categories");
+const categoriesLength = textAnimals.children.length;
+
+console.log(textAnimals.firstElementChild.lastElementChild.childNodes[1]);
+
+function getTotalOfCategory(value) {
+  let total = 0;
+  for (let i = 0; i < array.length; i++) {}
+}
+
+console.log("Number of categories: ", categoriesLength);
+
+console.log(
+  "Category: ",
+  textAnimals.firstElementChild.firstElementChild.textContent
+);
+console.log("Elements: ", 4);
+
+console.log(
+  "Category: ",
+  textAnimals.firstElementChild.nextElementSibling.firstElementChild.textContent
+);
+console.log("Elements: ", 3);
+
+console.log(
+  "Category: ",
+  textAnimals.lastElementChild.firstElementChild.textContent
+);
+console.log("Elements: ", 5);
