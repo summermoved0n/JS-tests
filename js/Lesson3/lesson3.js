ef29c7dbb355fc356f5aedee121934652a1fdeb7
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2024-11-02 20:58:50 -0400

    02/11 p2

--- js/Lesson3/lesson3.js
+++ js/Lesson3/lesson3.js
@@ -418,4 +418,357 @@ function Test20() {
 
 // ? Test 21
 
-function Test21() {}
+function Test21() {
+  const highTemperatures = {
+    yesterday: 28,
+    today: 26,
+    tomorrow: 33,
+  };
+  // Change code below this line
+
+  const { yesterday, today, tomorrow } = highTemperatures;
+
+  // Change code above this line
+  const meanTemperature = (yesterday + today + tomorrow) / 3;
+}
+
+// ? Test 22
+
+function Test22() {
+  const highTemperatures = {
+    yesterday: 28,
+    today: 26,
+    tomorrow: 33,
+  };
+  // Change code below this line
+
+  const {
+    yesterday,
+    today,
+    tomorrow,
+    icon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+  } = highTemperatures;
+
+  // Change code above this line
+  const meanTemperature = (yesterday + today + tomorrow) / 3;
+}
+
+// ? Test 23
+
+function Test23() {
+  const highTemperatures = {
+    yesterday: 28,
+    today: 26,
+    tomorrow: 33,
+  };
+  // Change code below this line
+
+  const {
+    yesterday: highYesterday,
+    today: highToday,
+    tomorrow: highTomorrow,
+    icon: highIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+  } = highTemperatures;
+
+  // Change code above this line
+  const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+}
+
+// ? Test 24
+
+function Test24() {
+  const colors = [
+    { hex: "#f44336", rgb: "244,67,54" },
+    { hex: "#2196f3", rgb: "33,150,243" },
+    { hex: "#4caf50", rgb: "76,175,80" },
+    { hex: "#ffeb3b", rgb: "255,235,59" },
+  ];
+
+  const hexColors = [];
+  const rgbColors = [];
+  // Change code below this line
+
+  for (const { hex, rgb } of colors) {
+    hexColors.push(hex);
+    rgbColors.push(rgb);
+  }
+}
+
+// ? Test 25
+
+function Test25() {
+  const forecast = {
+    today: {
+      low: 28,
+      high: 32,
+      icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+    },
+    tomorrow: {
+      low: 27,
+      high: 31,
+    },
+  };
+  // Change code below this line
+
+  const {
+    today: {
+      low: lowToday,
+      high: highToday,
+      icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+    },
+    tomorrow: {
+      low: lowTomorrow,
+      high: highTomorrow,
+      icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+    },
+  } = forecast;
+}
+
+// ? Test 26
+
+function Test26() {
+  // Change code below this line
+  function calculateMeanTemperature(forecast) {
+    const {
+      today: { low: todayLow, high: todayHigh },
+      tomorrow: { low: tomorrowLow, high: tomorrowHigh },
+    } = forecast;
+
+    // Change code above this line
+
+    return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+  }
+
+  calculateMeanTemperature({
+    today: { low: 28, high: 32 },
+    tomorrow: { low: 25, high: 29 },
+  });
+  calculateMeanTemperature({
+    today: { low: 37, high: 40 },
+    tomorrow: { low: 33, high: 38 },
+  });
+}
+
+// ? Test 27
+
+function Test27() {
+  const scores = [89, 64, 42, 17, 93, 51, 26];
+  // Change code below this line
+  const bestScore = Math.max(...scores);
+  const worstScore = Math.min(...scores);
+}
+
+// ? Test 28
+
+function Test28() {
+  const firstGroupScores = [64, 42, 93];
+  const secondGroupScores = [89, 14, 51, 26];
+  const thirdGroupScores = [29, 47, 18, 97, 81];
+  // Change code below this line
+  const allScores = [
+    ...firstGroupScores,
+    ...secondGroupScores,
+    ...thirdGroupScores,
+  ];
+  const bestScore = Math.max(...allScores);
+  const worstScore = Math.min(...allScores);
+}
+
+// ? Test 29
+
+function Test29() {
+  const defaultSettings = {
+    theme: "light",
+    public: true,
+    withPassword: false,
+    minNumberOfQuestions: 10,
+    timePerQuestion: 60,
+  };
+  const overrideSettings = {
+    public: false,
+    withPassword: true,
+    timePerQuestion: 30,
+  };
+  // Change code below this line
+  const finalSettings = { ...defaultSettings, ...overrideSettings };
+}
+
+// ? Test 30
+
+function Test30() {
+  function makeTask(data) {
+    const completed = false;
+    const category = "General";
+    const priority = "Normal";
+    // Change code below this line
+    return { completed, category, priority, ...data };
+    // Change code above this line
+  }
+
+  makeTask({});
+  makeTask({
+    category: "Homemade",
+    priority: "Low",
+    text: "Take out the trash",
+  });
+  makeTask({ category: "Finance", text: "Take interest" });
+  makeTask({ priority: "Low", text: "Choose shampoo" });
+  makeTask({ text: "Buy bread" });
+}
+
+// ? Test 31
+
+function Test31() {
+  // Change code below this line
+  function add(...args) {
+    // Change code above this line
+    let total = 0;
+    for (const arg of args) {
+      total += arg;
+    }
+    console.log(total);
+    return total;
+  }
+
+  add(15, 27);
+  add(12, 4, 11, 48);
+  add(32, 6, 13, 19, 8);
+  add(74, 11, 62, 46, 12, 36);
+}
+
+// ? Test 32
+
+function Test32() {
+  // Change code below this line
+  function addOverNum(value, ...args) {
+    let total = 0;
+
+    for (const arg of args) {
+      if (arg > value) {
+        total += arg;
+      }
+    }
+    return total;
+    // Change code above this line
+  }
+
+  addOverNum(50, 15, 27);
+  addOverNum(10, 12, 4, 11, 48, 10, 8);
+  addOverNum(15, 32, 6, 13, 19, 8);
+  addOverNum(20, 74, 11, 62, 46, 12, 36);
+}
+
+// ? Test 33
+
+function Test33() {
+  // Change code below this line
+  function findMatches(value, ...args) {
+    const matches = []; // Don't change this line
+    for (const arg of args) {
+      const number = value.includes(arg);
+      if (number) {
+        matches.push(arg);
+      }
+    }
+    // Change code above this line
+    return matches;
+  }
+  findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7);
+  findMatches([4, 89, 17, 36, 2], 8, 17, 89, 27, 2);
+  findMatches([10, 24, 41, 6, 9, 19], 24, 11, 9, 23, 41);
+  findMatches([63, 11, 8, 29], 4, 7, 16);
+}
+
+// ? Test 34
+
+function Test34() {
+  const bookShelf = {
+    // Change code below this line
+    books: ["The last kingdom", "The guardian of dreams"],
+    getBooks() {
+      return "Returning all books";
+    },
+    addBook(bookName) {
+      return `Adding book ${bookName}`;
+    },
+    removeBook(bookName) {
+      return `Deleting book ${bookName}`;
+    },
+    updateBook(oldName, newName) {
+      return `Updating book ${oldName} to ${newName}`;
+    },
+    // Change code above this line
+  };
+}
+
+// ? Test 35
+
+function Test35() {
+  const bookShelf = {
+    books: ["The last kingdom", "Haze", "The guardian of dreams"],
+    updateBook(oldName, newName) {
+      // Change code below this line
+      this.books.splice(this.books.indexOf(oldName), 1, newName);
+      // Change code above this line
+      return this.books;
+    },
+  };
+  bookShelf.updateBook("Haze", "Dungeon chronicles");
+  bookShelf.updateBook("The last kingdom", "Dune");
+}
+
+// ? Test 36, Test 37, Test 38, Test39, test 40, test 41
+
+const atTheOldToad = {
+  potions: [
+    { name: "Speed potion", price: 460 },
+    { name: "Dragon breath", price: 780 },
+    { name: "Stone skin", price: 520 },
+  ],
+  // Change code below this line
+  getPotions() {
+    return this.potions;
+  },
+
+  addPotion(newPotion) {
+    const { name: newPotionName } = newPotion;
+
+    for (const { name } of this.potions) {
+      if (name === newPotionName) {
+        console.log(
+          `Error! Potion ${newPotionName} is already in your inventory!`
+        );
+        return `Error! Potion ${newPotionName} is already in your inventory!`;
+      }
+    }
+
+    this.potions.push(newPotion);
+  },
+
+  removePotion(potionName) {
+    for (const potion of this.potions) {
+      if (potionName !== potion.name) {
+        continue;
+      }
+      const potionIndex = this.potions.indexOf(potion);
+      this.potions.splice(potionIndex, 1);
+    }
+    return `Potion ${potionName} is not in inventory!`;
+  },
+
+  updatePotionName(oldName, newName) {
+    for (const potion of this.potions) {
+      console.log(potion.name);
+      if (oldName !== potion.name) {
+        console.log("bad name");
+        continue;
+      }
+      console.log("good name");
+
+      const potionIndex = this.potions.indexOf(potion);
+      return (this.potions[potionIndex].name = newName);
+    }
+    return `Potion ${oldName} is not in inventory!`;
+  },
+  // Change code above this line
+};
