874954d9cbb5f3a2df7de023165a9da54221d235
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2025-10-22 20:03:28 -0400

    test

--- js/LeetCode/leetcode.js
+++ js/LeetCode/leetcode.js
@@ -32,3 +32,38 @@ var isPalindrome = function (x) {
 };
 
 isPalindrome(121); // true
+
+var romanToInt = function (s) {
+  const romanNumerals = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let total = 0;
+
+  for (let i = 0; i < s.length; i += 1) {
+    const currentVal = romanNumerals[s[i]];
+    const nextVal = romanNumerals[s[i + 1]];
+    if (nextVal && currentVal < nextVal) {
+      total -= currentVal;
+    } else {
+      total += currentVal;
+    }
+  }
+
+  return total;
+};
+
+var longestCommonPrefix = function (strs) {
+  let prefix;
+
+  for (let i = 0; i < strs.length; i += 1) {
+    console.log(strs[i].split(""));
+  }
+};
+longestCommonPrefix(["flower", "flow", "flight"]); // "fl"
